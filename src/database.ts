33146b83a4b3f5e5c56616fe4859853f6941a429
Layon Endlich Rodrigues <layonendlich@hotmail.com>
2026-04-21 18:55:59 -0300

    MySQL

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,34 @@
 import { knex as knexSetup } from "knex"
 import { env } from './env'
 
+var dbConnection: Object | String | null = null
+
+switch (env.DATABASE_CLIENT) {
+    case 'mysql':
+         dbConnection = {
+            host: '127.0.0.1',
+            port: 3306,
+            user: 'your_database_user',
+            password: 'your_database_password',
+            database: 'myapp_test',
+         }
+         break
+    case 'pg':
+        dbConnection = env.DATABASE_URL
+        break
+    case 'sqlite':
+        dbConnection = {
+            filename: env.DATABASE_URL || './app.db',
+        }
+        break
+    default:
+        throw new Error('Unsupported database client')
+}
+
 export const config = {
     client: env.DATABASE_CLIENT,
-    connection: env.DATABASE_URL,
-    useNullAsDefaul: true,
+    connection: dbConnection,
+    useNullAsDefault: true,
     migrations: {
         extension: 'ts',
         directory: env.DATABASE_MIGRATIONS
